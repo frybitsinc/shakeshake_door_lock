b8f32b7c900fd6dada774ad79c956de14569b273
frybits <frybitsofficial@gmail.com>
2017-10-17 19:13:58 +0900

    Settings menu added

--- app/src/main/java/com/blogspot/frybitsinc/shakeshack_unlock/MainActivity.java
+++ app/src/main/java/com/blogspot/frybitsinc/shakeshack_unlock/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
+    private Button mButtonSettings;
     private Button mButtonUnlock;
     private Button mButtonLock;
 
@@ -14,9 +15,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButtonSettings = (Button) findViewById(R.id.button_settings);
         mButtonUnlock = (Button) findViewById(R.id.button_unlock);
         mButtonLock = (Button) findViewById(R.id.button_lock);
 
+        mButtonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
         mButtonUnlock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
---
+++ app/src/main/java/com/blogspot/frybitsinc/shakeshack_unlock/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.blogspot.frybitsinc.shakeshack_unlock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by spong on 2017-10-17.
+ */
+
+public class SettingsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
